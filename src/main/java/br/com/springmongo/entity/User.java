105554399e9e5322549e09cb9a0ebb42dea83819
Gustavo Santos <gustavosci@live.com>
2018-07-30 21:22:20 -0300

    started MongoDB connection

--- src/main/java/br/com/springmongo/entity/User.java
+++ src/main/java/br/com/springmongo/entity/User.java
@@ -1,30 +1,35 @@
 package br.com.springmongo.entity;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Document(collection = "user")
 public class User implements Serializable {
 
     private static final long serialVersionUID = -5938868532986747353L;
 
-    private Integer id;
+    @Id
+    private String id;
     private String name;
     private String email;
 
     public User() {
     }
 
-    public User(Integer id, String name, String email) {
+    public User(String id, String name, String email) {
         this.id = id;
         this.name = name;
         this.email = email;
     }
 
-    public Integer getId() {
+    public String getId() {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(String id) {
         this.id = id;
     }
 
